58078037fdc18d9ec7986a0e2aa629a1218235b0
michal.chrzaszcz <mchrzaszcz90@gmail.com>
2020-06-12 13:02:12 +0200

    Change placeholder elements to optional. Make banner markup dynamically.

--- src/script.js
+++ src/script.js
@@ -2,9 +2,15 @@
 
     let initRotator = function (linkElement, readOnlyKey) {
 
-        let imgElement = linkElement.getElementsByTagName('img')[0];
-        if (!imgElement) {
-            throw new Error(`Child element img of #${linkElement} not found`);
+        let createBanner = (src, title, link) => {
+            let anchor = document.createElement("a");
+            let image = document.createElement("img");
+            anchor.href = link;
+            anchor.title = title;
+            image.src = src;
+            image.alt = title;
+            anchor.append(image);
+            return anchor;
         }
 
         fetch(`https://api.flotiq.com/api/v1/content/rotator/rotator-986160?hydrate=1&auth_token=${readOnlyKey}`)
@@ -13,10 +19,11 @@
                 let enabledBanners = rotator.Banners.filter((banner) => banner.enabled === true);
                 let randomBanner = enabledBanners[Math.floor(Math.random() * enabledBanners.length)];
                 if (enabledBanners.length) {
-                    imgElement.src = `https://api.flotiq.com/image/${rotator.Width}x${rotator.Height}/${randomBanner.image[0].id}.${randomBanner.image[0].extension}`;
-                    imgElement.alt = randomBanner.title;
-                    linkElement.href = randomBanner.link;
-                    linkElement.title = randomBanner.title;
+                    linkElement.innerHTML = '';
+                    linkElement.append(createBanner(
+                        `https://api.flotiq.com/image/${rotator.Width}x${rotator.Height}/${randomBanner.image[0].id}.${randomBanner.image[0].extension}`,
+                        randomBanner.title,
+                        randomBanner.link));
                 }
             });
     }
